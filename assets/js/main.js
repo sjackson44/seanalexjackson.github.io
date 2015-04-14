80611e72be53aab3f46bf83a38807a3158354e93
Sean Jackson <sjackson44@gmail.com>
2015-04-14 15:35:39 -0400

    mobile detector

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,27 @@ $(window).on('scroll',function(){
 });
 
 
+function detectmob() { 
+ if( navigator.userAgent.match(/Android/i)
+ || navigator.userAgent.match(/webOS/i)
+ || navigator.userAgent.match(/iPhone/i)
+ || navigator.userAgent.match(/iPad/i)
+ || navigator.userAgent.match(/iPod/i)
+ || navigator.userAgent.match(/BlackBerry/i)
+ || navigator.userAgent.match(/Windows Phone/i)
+ ){
+    return true;
+  }
+ else {
+    return false;
+  }
+}
+
+if(window.detectmob()===true){
+	$('#circle').css("visibility", "hidden");
+}
+
+
 
    $(document).ready(function(){
       $('.parallax').parallax();
