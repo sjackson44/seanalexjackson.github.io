8a3697010a78706d839e8d634d309918d751d5e2
Sean Jackson <sjackson44@gmail.com>
2015-04-14 22:16:34 -0400

    stupid svgs that won't resize

--- assets/js/views/main.app.js
+++ assets/js/views/main.app.js
@@ -5,9 +5,32 @@ main.App = Backbone.View.extend({
 	mainTmp: _.template($('#main-template').html()),
 	initialize: function(){
 		this.render();
+		this.detect();
+	},
+
+	events: {
+		"window scroll" : "highlight"
 	},
 	render: function(){
 		$("#container").html(this.mainTmp());
 		return this;
 	},
+
+	detect: function detectmob() { 
+		 if( navigator.userAgent.match(/Android/i)
+		 || navigator.userAgent.match(/webOS/i)
+		 || navigator.userAgent.match(/iPhone/i)
+		 || navigator.userAgent.match(/iPad/i)
+		 || navigator.userAgent.match(/iPod/i)
+		 || navigator.userAgent.match(/BlackBerry/i)
+		 || navigator.userAgent.match(/Windows Phone/i)
+		 ){
+		 	$('#circle').css("visibility", "hidden");
+		    return true;
+		 }
+		 else {
+		    return false;
+		}
+	},
+
 })
