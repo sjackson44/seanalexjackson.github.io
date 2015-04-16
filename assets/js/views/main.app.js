a7b4b11835fa30c895d0e09c1b5ded7b4866e17d
Sean Jackson <sjackson44@gmail.com>
2015-04-16 13:09:59 -0400

    added follow me icons

--- assets/js/views/main.app.js
+++ assets/js/views/main.app.js
@@ -33,30 +33,27 @@ main.App = Backbone.View.extend({
 		 || navigator.userAgent.match(/Windows Phone/i)
 		 ){
 		 	$('#circle').css("visibility", "hidden");
-		    $('.row1').attr("id","devicon-mobile-row1")
-        	$('.row2').attr("id","devicon-mobile-row2")
+		    $('.row1').attr("id","devicon-mobile-row1");
+        	$('.row2').attr("id","devicon-mobile-row2");
 		    return true;
 		 }
 		 else {
-		    return false;
-		    $('.row1').removeAttr("devicon-mobile-row1");
+		 	$('.row1').removeAttr("devicon-mobile-row1");
         	$('.row2').removeAttr("devicon-mobile-row2");
+		    return false;
 		}
 	},
 
 	highlight: function() {
 			// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
 			var mainbottom = $('#main').offset().top + $('#main').height();
-
-
 			// on scroll, 
 			$(window).on('scroll',function(){
 
 		    // we round here to reduce a little workload
 		    stop = Math.round($(window).scrollTop());
 		    if (stop > mainbottom + 280) {
-		    	
-		        $('#nav-list').addClass('past-main');
+		    	$('#nav-list').addClass('past-main');
 		    } else {
 		        $('#nav-list').removeClass('past-main');
 		   }
