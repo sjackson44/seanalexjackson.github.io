a54ba1c3a5501e3880663c6151176e13d4920176
sjackson44 <sjackson44@gmail.com>
2015-09-11 01:15:37 -0400

    redesigned the redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,18 @@
 $(document).ready(function() {
-  $('.parallax').parallax();
-
-  //keeps track of what button has been hit
-  var counter = 0
-  //hides overlapping divs #about is shown first by default
-  $('#resume').hide();
-
-  //Original width of the window
-  var Initwidth = $(window).width();
-
-  //identifies the type margin-top correction applied before resize
-  var typeFix = true;
-
-  //button click behavior
-
- 	//ADD ANOTHER COUNTER TO KEEP SUB AND ADD EQUAL
-  $('#ab').click(function() {
-  	if (counter === 1){
- 			$('#resume').fadeOut();
-	  	// $('#projects').fadeOut();
-	  	$('#about').show('slow');
-	  	counter = 0;
-      typeFix = false;
-  	}
-  });
-
-  $('#res').click(function() {
-  	if (counter === 0){
-	  	$('#about').fadeOut();
-	  	// $('#projects').fadeOut();
-	  	$('#resume').show('slow');
-	  	counter = 1;
-  	}
-  });
-
-  $('#projects').click(function() {
-  	// $('#about').fadeOut();
-  	$('#resume').fadeOut();
+  // $('.parallax').parallax();
+  var ret = 0;
+  $('#screen').click(function() {
+    if (ret === 0) {
+      $('#title').fadeOut();
+      $('#nav').removeClass('hidden');
+      $('#nav').addClass('visible');
+      $('#info-container').removeClass('hidden');
+      $('#info-container').addClass('visible');
+      ret = 1;
+    } else if (ret === 4) {
+      $('#title').fadeIn();
+        $('#nav').css('visibility','hidden');
+      ret = 0;
+    }
   });
 });
