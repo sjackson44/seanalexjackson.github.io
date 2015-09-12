1e822c9a501741ae9fc1bb45b1046174daf60331
sjackson44 <sjackson44@gmail.com>
2015-09-11 22:00:42 -0400

    found higher res star tree gif, added algorithm to add and remove info from info-container

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,41 @@
 $(document).ready(function() {
-  // $('.parallax').parallax();
   var ret = 0;
+  var trace;
+  var id;
+  var arr = [];
+  var linkObj = {
+    ab: 'about',
+    res: 'resume',
+    proj: 'projects',
+    con: 'contact'
+  };
+
   $('#screen').click(function() {
     if (ret === 0) {
       $('#title').fadeOut();
-      $('#nav').removeClass('hidden');
-      $('#nav').addClass('visible');
-      $('#info-container').removeClass('hidden');
-      $('#info-container').addClass('visible');
-      ret = 1;
-    } else if (ret === 4) {
+      $('#nav').removeClass('hidden').addClass('visible').fadeIn();
+      $('#info-container').removeClass('hidden').addClass('visible').fadeIn('slow');
+      ret = 5;
+    } else if (ret === 5) {
       $('#title').fadeIn();
-        $('#nav').css('visibility','hidden');
+        $('#nav').removeClass('visible').addClass('hidden').fadeOut();
+        $('#info-container').removeClass('visible').addClass('hidden').fadeOut();
       ret = 0;
     }
   });
+  
+  //button click functions
+  $('a').hover(function() {
+    id = this.id;
+
+    $('#' + id).click(function() {
+      for(var rem in linkObj) {
+        if (rem !== id) {
+          $('#' + linkObj[rem]).removeClass('visible').addClass('hidden');
+        }
+      }
+      $('#' + linkObj[id]).removeClass('hidden').addClass('visible');  
+    });//ends click
+  });//ends hover
+
 });
